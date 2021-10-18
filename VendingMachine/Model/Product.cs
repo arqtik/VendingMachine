36b3ea018beb6da9a927f8f6669b9e8122be96c9
arqtik <martynascamets1@gmail.com>
2021-10-18 13:15:23 +0200

    Create Product.cs

---
+++ VendingMachine/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace VendingMachine.Model
+{
+    public abstract class Product
+    {
+        /// <summary>
+        /// Examine the products price and info
+        /// </summary>
+        /// <returns>Returns products price and info</returns>
+        public abstract string Examine();
+        
+        /// <summary>
+        /// Use the product
+        /// </summary>
+        /// <returns>Returns a string as the result of usage</returns>
+        public abstract string Use();
+    }
+}
