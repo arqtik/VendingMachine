93dee9cd55ac0ae4bdeff0a1af5a0c40c695fcf9
arqtik <martynascamets1@gmail.com>
2021-10-18 13:07:41 +0200

    Create IVending.cs
    Initial prototype

---
+++ VendingMachine/IVending.cs
@@ -0,0 +1,25 @@
+﻿namespace VendingMachine
+{
+    public interface IVending
+    {
+        /// <summary>
+        /// Purchase a product
+        /// </summary>
+        public void Purchase();
+        
+        /// <summary>
+        /// Shows all products available
+        /// </summary>
+        public string ShowAll();
+        
+        /// <summary>
+        /// Add money to the money pool
+        /// </summary>
+        public void InsertMoney();
+        
+        /// <summary>
+        /// Returns money left from the transaction in appropriate amount of change.
+        /// </summary>
+        public int EndTransaction();
+    }
+}
