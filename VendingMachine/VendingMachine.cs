faf75612a45349ef4f64d6d970eaf9a22e2ff528
arqtik <martynascamets1@gmail.com>
2021-10-18 13:15:30 +0200

    Create VendingMachine.cs

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,27 @@
+﻿namespace VendingMachine
+{
+    public class VendingMachine: IVending
+    {
+        public int MoneyPool { get; set; }
+
+        public void Purchase()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ShowAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertMoney(int money)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int EndTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
