51abd39702d7fed6dd1b3556e7dd24a3588fb31d
arqtik <martynascamets1@gmail.com>
2021-10-19 12:31:30 +0200

    Update NotEnoughMoneyException.cs

--- VendingMachine/NotEnoughMoneyException.cs
+++ VendingMachine/NotEnoughMoneyException.cs
@@ -4,6 +4,7 @@ namespace VendingMachine.Model
 {
     public class NotEnoughMoneyException : Exception
     {
-        public NotEnoughMoneyException(string message) : base(message){}
+        public NotEnoughMoneyException(int productPrice, int moneyAvailable) 
+            : base($"Not enough money. Product costs {productPrice}kr and {moneyAvailable}kr is available.") { }
     }
 }
