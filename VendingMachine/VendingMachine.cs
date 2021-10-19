db7e94b6b9a61c8c85acc1c1f9bb3ea6ff551237
arqtik <martynascamets1@gmail.com>
2021-10-19 12:31:46 +0200

    Update VendingMachine.cs
    Added comments for methods

--- VendingMachine/VendingMachine.cs
+++ VendingMachine/VendingMachine.cs
@@ -12,9 +12,15 @@ namespace VendingMachine
         private readonly int[] _denominations;
         private Product[] _products;
         
+        /// <summary>
+        /// Current amount of inserted money that is left
+        /// </summary>
         public int MoneyPool { get; private set; }
         
-        
+        /// <summary>
+        /// Create a new Vending Machine instance
+        /// </summary>
+        /// <param name="products">Array of products available in the vending machine</param>
         public VendingMachine(Product[] products)
         {
             _denominations = new []
@@ -25,6 +31,13 @@ namespace VendingMachine
             _products = products;
         }
 
+        /// <summary>
+        /// Purchase a product from the vending machine using the money inserted
+        /// </summary>
+        /// <param name="productIndex">The index of the product you want to purchase</param>
+        /// <returns>Returns reference to the product purchased</returns>
+        /// <exception cref="ArgumentException">If the product index does not exist</exception>
+        /// <exception cref="NotEnoughMoneyException">If not enough money in the money pool to purchase product</exception>
         public Product Purchase(int productIndex)
         {
             if (productIndex > 0 && productIndex < _products.Length)
@@ -38,15 +51,19 @@ namespace VendingMachine
                 }
                 else
                 {
-                    throw new ArgumentException("There is not enough money in the pool to buy this product");
+                    throw new NotEnoughMoneyException(product.Price, MoneyPool);
                 }
             }
             else
             {
-                throw new ArgumentException("Product chosen does not exist");
+                throw new ArgumentException("Product index chosen does not exist");
             }
         }
-
+        
+        /// <summary>
+        /// Shows all of the products available in the vending machine with their index
+        /// </summary>
+        /// <returns>Returns a string with all the products and their indexes, could contain multiple lines</returns>
         public string ShowAll()
         {
             StringBuilder sb = new StringBuilder();
@@ -59,6 +76,11 @@ namespace VendingMachine
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Insert money into the vending machine
+        /// </summary>
+        /// <param name="money">The money unit you want to insert. Should be of a valid denomination</param>
+        /// <exception cref="ArgumentException">Throws exception if invalid denomination input</exception>
         public void InsertMoney(int money)
         {
             if (_denominations.Contains(money))
@@ -71,6 +93,10 @@ namespace VendingMachine
             }
         }
 
+        /// <summary>
+        /// Ends the current transaction
+        /// </summary>
+        /// <returns>Returns array of all denominations that should be returned</returns>
         public int[] EndTransaction()
         {
             List<int> change = new List<int>();
