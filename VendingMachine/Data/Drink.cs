8c08280f9e08d3af826ddc326a326d94c3262659
arqtik <martynascamets1@gmail.com>
2021-10-19 12:34:49 +0200

    Update products
    Added comments to products

--- VendingMachine/Data/Drink.cs
+++ VendingMachine/Data/Drink.cs
@@ -4,16 +4,27 @@ namespace VendingMachine.Data
 {
     public class Drink : Product
     {
-        public Drink(string name, int price) : base(name, price)
-        {
-            
-        }
         
+        /// <summary>
+        /// Create a new instance of a drink product
+        /// </summary>
+        /// <param name="name">The name of the drink</param>
+        /// <param name="price">The price of the drink</param>
+        public Drink(string name, int price) : base(name, price) { }
+        
+        /// <summary>
+        /// Examine this product
+        /// </summary>
+        /// <returns>Returns a string with the Name, price and info about the product</returns>
         public override string Examine()
         {
             return base.Examine() + $"\n{Name} is a drink, a good one to quench your thirst with.";
         }
 
+        /// <summary>
+        /// Use the product
+        /// </summary>
+        /// <returns>Returns a string with how you use the product</returns>
         public override string Use()
         {
             return $"You drink the {Name}, it really hits the spot.";
--- VendingMachine/Data/Toy.cs
+++ VendingMachine/Data/Toy.cs
@@ -4,16 +4,26 @@ namespace VendingMachine.Data
 {
     public class Toy : Product
     {
-        public Toy(string name, int price) : base(name, price)
-        {
-            
-        }
+        /// <summary>
+        /// Create a new instance of a toy product
+        /// </summary>
+        /// <param name="name">The name of the toy</param>
+        /// <param name="price">The price of the toy</param>
+        public Toy(string name, int price) : base(name, price) { }
 
+        /// <summary>
+        /// Examine this product
+        /// </summary>
+        /// <returns>Returns a string with the Name, price and info about the product</returns>
         public override string Examine()
         {
             return base.Examine() + $"\n{Name} is a toy, it looks pretty fun!";
         }
 
+        /// <summary>
+        /// Use the product
+        /// </summary>
+        /// <returns>Returns a string with how you use the product</returns>
         public override string Use()
         {
             return $"You play with the {Name}";
