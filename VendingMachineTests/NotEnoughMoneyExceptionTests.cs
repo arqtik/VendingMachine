eda48cd83dbc4452fbd8e09f0620f5742b174472
arqtik <martynascamets1@gmail.com>
2021-10-19 13:00:48 +0200

    Create NotEnoughMoneyExceptionTests.cs

---
+++ VendingMachineTests/NotEnoughMoneyExceptionTests.cs
@@ -0,0 +1,19 @@
+﻿using VendingMachine.Exceptions;
+using Xunit;
+
+namespace VendingMachineTests
+{
+    public class NotEnoughMoneyExceptionTests
+    {
+        [Fact]
+        private void NotEnoughMoneyExceptionTest()
+        {
+            int productPrice = 10;
+            int availableMoney = 5;
+
+            NotEnoughMoneyException ex = new NotEnoughMoneyException(productPrice, availableMoney);
+            
+            Assert.Equal($"Not enough money. Product costs {productPrice}kr and {availableMoney}kr is available.", ex.Message);
+        }
+    }
+}
