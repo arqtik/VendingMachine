7507f87af37634587114c6569d6c99466a0b4d4e
arqtik <martynascamets1@gmail.com>
2021-10-19 12:52:01 +0200

    Added tests

---
+++ VendingMachineTests/VendingMachineTests.cs
@@ -0,0 +1,32 @@
+using System;
+using VendingMachine.Model;
+using Xunit;
+
+namespace VendingMachineTests
+{
+    public class VendingMachineTests : IDisposable
+    {
+        private Product[] _products;
+        
+        public VendingMachineTests()
+        {
+            _products =
+            {
+                
+            }
+        }
+        
+        public void Dispose()
+        {
+            
+        }
+        
+        [Fact]
+        public void VendingMachineTest()
+        {
+            
+        }
+
+        
+    }
+}
